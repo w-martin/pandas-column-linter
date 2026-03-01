61ec658bc408b1fe1b480b4d67d0173b077b1363
William Martin <will.st4@gmail.com>
2026-03-01 19:32:03 +0000

    Bump version to 0.2.1 and add Python 3.11–3.14 classifiers in `pyproject.toml`.

--- src/typedframes/__init__.py
+++ src/typedframes/__init__.py
@@ -37,7 +37,7 @@ Usage:
     df.filter(UserSchema.user_id.col > 10)  # ✓ Refactor-safe polars expression
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .base_schema import BaseSchema as BaseSchema
 from .column import Column as Column
